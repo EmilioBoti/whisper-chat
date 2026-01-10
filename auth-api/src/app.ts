05c945a3fbbeeca4cf3b8f519f0ef46ca9a5f148
Emilio Botier <emiliobotier@gmail.com>
2026-01-10 17:57:55 +0100

    feat: add proxy with https

--- auth-api/src/app.ts
+++ auth-api/src/app.ts
@@ -6,12 +6,14 @@ import { errorMiddleware } from './middleware/error.middleware.js'
 
 const app = express()
 
+const port = parseInt(process.env.AUTH_SERVER_PORT || "3001") || 3001
 const envFile = process.env.NODE_ENV === 'production' 
   ? '.env.production' 
   : '.env.development'
 
 dotenv.config({path: envFile })
 
+app.set('trust proxy', true) // trust first proxy
 //Middleware
 app.use(express.json())
 app.use(errorMiddleware)
@@ -20,14 +22,13 @@ app.use(errorMiddleware)
 app.get('/api/healty', (req: Request, res: Response) => { res.send('Auth server is running!') })
 app.use('/api/', authRoutes)
 
-
 /**
  * MUST BE LAST
  * Capture error 
  */
 app.use(errorMiddleware)
 
-app.listen(process.env.AUTH_SERVER_PORT, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port: ${process.env.AUTH_SERVER_PORT}`)
 })
 
