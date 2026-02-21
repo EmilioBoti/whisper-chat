c5020558dffcffbdec75b9e2031c8695180dd886
Emilio Botier <70599464+EmilioBoti@users.noreply.github.com>
2026-02-21 01:19:47 +0100

    feat: add initConnection event and message dto

--- chat-ws/src/app.ts
+++ chat-ws/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express'
 import { createServer } from 'http'
 import dotenv from 'dotenv'
-import { SocketServer } from './socket/SocketServer.ts'
-import { registeSocketEvent } from './socket/events/register.event.ts'
+import { SocketServer } from './socket/SocketServer.js'
+import { initConnection } from './socket/events/initConnection.js'
+import chatRoutes from './routes/chat.route.js'
+import { errorMiddleware } from './middleware/error.middleware.js'
 
 const port = Number(process.env.CHAT_WS_SERVER_PORT) || 3002
 const envFile = process.env.NODE_ENV === 'production' 
@@ -24,13 +26,19 @@ app.use(express.json())
 
 //API Endpoints
 app.get('/ws/health', (req: Request, res: Response) => { res.send('Chat WebSocket Server is running!!!') })
+app.use('/ws/chat', chatRoutes)
 
 /**
  * Register user connection and events
  * @argument Server
  */
-registeSocketEvent(io)
+initConnection(io)
 
+/**
+ * MUST BE LAST
+ * Capture error 
+ */
+app.use(errorMiddleware)
 
 server.listen(port, "0.0.0.0", () => {
   console.log(`Chat WebSocket Server is running on port: ${port}`)
---
+++ chat-ws/src/models/dto/messange.dto.ts
@@ -0,0 +1,7 @@
+export interface MessageSentDTO {
+  sender: string,
+  receiver: string,
+  chatId: string,
+  content: string,
+  date: string
+}
