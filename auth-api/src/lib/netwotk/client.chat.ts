e24b7883a8b57b12ebe0db2f0288ce56869fbff2
Emilio Botier <erodriguez@napptilus.com>
2026-07-04 15:50:25 +0200

    fix: create user path

--- auth-api/src/lib/netwotk/client.chat.ts
+++ auth-api/src/lib/netwotk/client.chat.ts
@@ -4,7 +4,7 @@ import InternalService from './ClientService.js'
 const service = new InternalService(process.env.CLIENT_CHAT_BASE_URL || '')
 
 export const createProfile = async (id: string, name: string): Promise<SimpleProfile> => {
-  return await service.request<SimpleProfile>('/ws/user/create-profile', {
+  return await service.request<SimpleProfile>('/user/create-profile', {
     method: 'POST',
     body: JSON.stringify({ id, name }),
   })
